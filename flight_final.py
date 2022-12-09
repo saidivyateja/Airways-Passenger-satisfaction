b5d2934230324edc1263e426a8f5223e971cdaf6
Konda Sai Divya Teja <69079789+saidivyateja@users.noreply.github.com>
2022-12-09 00:53:04 -0500

    Add files via upload

--- flight_final.py
+++ flight_final.py
@@ -361,18 +361,3 @@ page_names_to_funcs = {
 
 selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
 page_names_to_funcs[selected_page]()
-
-
-
-
-
-
-    
-
-
-# with tab3:
-    
-
-# with tab4:
-    
-#     #user_pred  = clf.predict(np.array(fly)[:,:])
