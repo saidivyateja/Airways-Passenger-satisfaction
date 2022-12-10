1df78ebca2e1b18b64dd99d1d5b8427c00e6416a
Konda Sai Divya Teja <69079789+saidivyateja@users.noreply.github.com>
2022-12-09 23:09:25 -0500

    Add files via upload

--- flight_final.py
+++ flight_final.py
@@ -48,56 +48,34 @@ def page1():
     st.header("Contents of the Dataset")
 
     st.markdown('''
-    Gender: Gender of the passengers (Female, Male)
-
-    Customer Type: The customer type (Loyal customer, disloyal customer)
-
-    Age: The actual age of the passengers
-
-    Type of Travel: Purpose of the flight of the passengers (Personal Travel, Business Travel)
-
-    Class: Travel class in the plane of the passengers (Business, Eco, Eco Plus)
-
-    Flight distance: The flight distance of this journey
-
-    Inflight wifi service: Satisfaction level of the inflight wifi service (0:Not Applicable;1-5)
-
-    Departure/Arrival time convenient: Satisfaction level of Departure/Arrival time convenient
-
-    Ease of Online booking: Satisfaction level of online booking
-
-    Gate location: Satisfaction level of Gate location
-
-    Food and drink: Satisfaction level of Food and drink
-
-    Online boarding: Satisfaction level of online boarding
-
-    Seat comfort: Satisfaction level of Seat comfort
-
-    Inflight entertainment: Satisfaction level of inflight entertainment
-
-    On-board service: Satisfaction level of On-board service
-
-    Leg room service: Satisfaction level of Leg room service
-
-    Baggage handling: Satisfaction level of baggage handling
-
-    Check-in service: Satisfaction level of Check-in service
-
-    Inflight service: Satisfaction level of inflight service
-
-    Cleanliness: Satisfaction level of Cleanliness
-
-    Departure Delay in Minutes: Minutes delayed when departure
-
-    Arrival Delay in Minutes: Minutes delayed when Arrival
-
-    Satisfaction: Airline satisfaction level(Satisfaction, neutral or dissatisfaction)
+    Gender: Gender of the passengers (Female, Male)  
+    Customer Type: The customer type (Loyal customer, disloyal customer)  
+    Age: The actual age of the passengers  
+    Type of Travel: Purpose of the flight of the passengers (Personal Travel, Business Travel)  
+    Class: Travel class in the plane of the passengers (Business, Eco, Eco Plus)  
+    Flight distance: The flight distance of this journey  
+    Inflight wifi service: Satisfaction level of the inflight wifi service (0:Not Applicable;1-5)  
+    Departure/Arrival time convenient: Satisfaction level of Departure/Arrival time convenient  
+    Ease of Online booking: Satisfaction level of online booking  
+    Gate location: Satisfaction level of Gate location  
+    Food and drink: Satisfaction level of Food and drink  
+    Online boarding: Satisfaction level of online boarding  
+    Seat comfort: Satisfaction level of Seat comfort  
+    Inflight entertainment: Satisfaction level of inflight entertainment  
+    On-board service: Satisfaction level of On-board service  
+    Leg room service: Satisfaction level of Leg room service  
+    Baggage handling: Satisfaction level of baggage handling  
+    Check-in service: Satisfaction level of Check-in service  
+    Inflight service: Satisfaction level of inflight service  
+    Cleanliness: Satisfaction level of Cleanliness  
+    Departure Delay in Minutes: Minutes delayed when departure  
+    Arrival Delay in Minutes: Minutes delayed when Arrival  
+    Satisfaction: Airline satisfaction level(Satisfaction, neutral or dissatisfaction)  
         ''')
 
 def page2():
 
-    st.subheader('''Here is the heatmap of all the columns based on the correlation with CO2_Emissions''')
+    st.subheader('''Here is the heatmap of all the columns based on the correlation with satisfaction column''')
 
     cdf = df[['Inflight wifi service','Departure/Arrival time convenient','Ease of Online booking','Gate location','Food and drink','Online boarding','Seat comfort','Inflight entertainment','On-board service','Leg room service','Baggage handling','Checkin service','Inflight service','Cleanliness','Departure Delay in Minutes','Arrival Delay in Minutes']]
 
@@ -144,7 +122,7 @@ def page2():
 #         st.write("You selected:")
 #         st.json(selection["selected_rows"])
 
-    st.subheader('''An interactive plot between columns of the car crash dataset''')
+    st.subheader('''An interactive plot between columns of the dataset''')
 
     x_axis_choice = st.selectbox(
         "x axis",
